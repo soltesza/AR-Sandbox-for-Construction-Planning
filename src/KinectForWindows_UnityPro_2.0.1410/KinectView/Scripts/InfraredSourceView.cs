3de94829469bd1019bcb7ee94663ce4cdcc6360f
Andrew Soltesz <soltesza@users.noreply.github.com>
2018-01-24 23:36:43 -0800

    Added Kinect plugin for Unity

---
+++ src/KinectForWindows_UnityPro_2.0.1410/KinectView/Scripts/InfraredSourceView.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InfraredSourceView : MonoBehaviour 
+{
+    public GameObject InfraredSourceManager;
+    private InfraredSourceManager _InfraredManager;
+    
+    void Start () 
+    {
+        gameObject.renderer.material.SetTextureScale("_MainTex", new Vector2(-1, 1));
+    }
+    
+    void Update()
+    {
+        if (InfraredSourceManager == null)
+        {
+            return;
+        }
+        
+        _InfraredManager = InfraredSourceManager.GetComponent<InfraredSourceManager>();
+        if (_InfraredManager == null)
+        {
+            return;
+        }
+    
+        gameObject.renderer.material.mainTexture = _InfraredManager.GetInfraredTexture();
+    }
+}
