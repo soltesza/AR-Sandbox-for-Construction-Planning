3c191d757189785c477f8379bd66f073a289fb02
Andrew Soltesz <soltesza@users.noreply.github.com>
2018-02-02 14:40:36 -0800

    Added draggable cut/fill panel

---
+++ src/AR Sandbox/Assets/Scripts/PanelDrag.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelDrag : MonoBehaviour {
+	private Vector3 delta;
+
+	public void BeginDrag() {
+		delta.x = transform.position.x - Input.mousePosition.x;
+		delta.y = transform.position.y - Input.mousePosition.y;
+		delta.z = 0f;
+	}
+
+	public void OnDrag() {
+		transform.position = Input.mousePosition + delta;
+	}
+}
