44daa2e749736b842775aeb93b780b24b2b83cce
petroffr <petroffr@oregonstate.edu>
2018-02-03 19:15:35 -0800

    Created UI Manager script
    Created a UI Manager script and cut and fill manager script to give both
    the buttons and the cut and fill panel functionality.
    For now, buttons simply write a string to the debug console.

---
+++ src/AR_Sandbox/Assets/Scripts/UIManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.SceneManagement; // for restart function
+
+// https://www.studica.com/blog/create-ui-unity-tutorial
+
+using UnityEngine.UI; //Need this for calling UI scripts
+
+
+public class UIManager : MonoBehaviour {
+
+    [SerializeField]
+    Transform UIPanel; //Will assign our panel to this variable so we can enable/disable it
+
+    [SerializeField]
+    Transform CutAndFillPanel; //Will assign our panel to this variable so we can enable/disable it
+
+    bool isPaused; //Used to determine paused state
+
+    void Start()
+    {
+        UIPanel.gameObject.SetActive(false); //make sure our pause menu is disabled when scene starts
+        CutAndFillPanel.gameObject.SetActive(false);
+        isPaused = false; //make sure isPaused is always false when our scene opens
+    }
+
+    void Update()
+    {
+        //If player presses escape and game is not paused. Pause game. If game is paused and player presses escape, unpause.
+        if (Input.GetKeyDown(KeyCode.Escape) && !isPaused)
+            Pause();
+        else if (Input.GetKeyDown(KeyCode.Escape) && isPaused)
+            UnPause();
+    }
+
+    public void Depth()
+    {
+        Debug.Log("Depth Mode Clicked");
+    }
+
+    public void Design()
+    {
+        Debug.Log("Design Mode Clicked");
+    }
+
+    public void CutAndFill()
+    {
+        Debug.Log("Cut and Fill Mode Clicked");
+
+        if (CutAndFillPanel.gameObject.activeSelf)
+            CutAndFillPanel.gameObject.SetActive(false);
+        else
+            CutAndFillPanel.gameObject.SetActive(true);
+    }
+
+    public void Calibrate()
+    {
+        Debug.Log("Calibrate Mode Clicked");
+    }
+
+    public void Pause()
+    {
+        isPaused = true;
+        UIPanel.gameObject.SetActive(true); //turn on the pause menu
+    }
+
+    public void UnPause()
+    {
+        isPaused = false;
+        UIPanel.gameObject.SetActive(false); //turn off pause menu
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
