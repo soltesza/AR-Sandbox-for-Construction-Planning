2aa77a3fd36677d8b4f17ea53f310db1b72192a0
Andrew Soltesz <ansolt@outlook.com>
2018-02-05 00:08:35 -0800

    Added calibraton mode area selector

---
+++ src/AR_Sandbox/Assets/Scripts/PanelResize.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PanelResize : MonoBehaviour {
+    private Vector2 initialMousePos, initialDeltaSize;
+
+    public RectTransform rectTransform;
+
+    void Start() {
+        if (!rectTransform)
+            Debug.LogError("PanelResize: no RectTransform selected!");
+    }
+
+    public void BeginDrag() {
+        initialMousePos = Input.mousePosition;
+        initialDeltaSize = rectTransform.sizeDelta;
+    }
+
+    public void OnDrag() {
+        Vector2 mouseDelta = (Vector2)initialMousePos - (Vector2)Input.mousePosition;
+        mouseDelta = new Vector2(-mouseDelta.x, mouseDelta.y);
+        mouseDelta += initialDeltaSize;
+        rectTransform.sizeDelta = mouseDelta;
+    }
+}
