f3b51ea579685c09c1cb3b4fd7f2a297729dfb5b
sprouseamark <sprouseamark@gmail.com>
2018-02-06 12:22:00 -0800

    Created Mode Manager Script and new type for display modes

---
+++ src/AR_Sandbox/Assets/ModeManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Use this to keep track of what state we are in
+//Should other objects find the current state here?  Should this manager what is called for each state?
+
+public enum DisplayMode : short {Depth, CutFill, Design, Calibrate};
+
+public class ModeManager : MonoBehaviour {
+
+    public static ModeManager instance = null;
+    public DisplayMode dMode = DisplayMode.Depth;
+
+	void Awake () {
+        //Ensures that this object is a singleton
+        if (instance == null)
+            instance = this;
+        else if (instance != this)
+            Destroy(gameObject);
+	}
+	
+	
+	void Update () {
+		
+	}
+}
