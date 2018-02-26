f14f9dc88f93c8089b813957a362873774ada79b
Andrew Soltesz <ansolt@outlook.com>
2018-02-25 23:48:01 -0800

    Added ability to edit bezier curves at runtime

--- src/AR_Sandbox/Assets/Scripts/Road.cs
+++ src/AR_Sandbox/Assets/Scripts/Road.cs
@@ -4,45 +4,51 @@ using UnityEngine;
 
 [RequireComponent(typeof(LineRenderer))]
 public class Road : MonoBehaviour {
+	public RoadControlPoint controlPointTemplate;
+
 	private LineRenderer lineRenderer;
-	private List<Vector3> points;
+	private List<RoadControlPoint> controlPoints;
 
 	private const int SEGMENT_COUNT = 20; // Number of line segments per curve, increase this number for a smoother line
 
 	// Use this for initialization
 	void Start () {
 		lineRenderer = GetComponent<LineRenderer> ();
-		points = new List<Vector3> ();
-		points.Add (new Vector3(0f, 0f, 0f));
-		points.Add (new Vector3 (1f, 4f, 6f));
-		points.Add (new Vector3 (5f, -2f, 6f));
-		points.Add (new Vector3(7f, 0f, 7f));
+		controlPoints = new List<RoadControlPoint> ();
+
+		CreateControlPoint (new Vector3(0f, 0f, 0f));
+		CreateControlPoint (new Vector3 (1f, 4f, 6f));
+		CreateControlPoint (new Vector3 (5f, -2f, 6f));
+		CreateControlPoint (new Vector3(7f, 0f, 7f));
 
 		lineRenderer.positionCount = SEGMENT_COUNT;
 
 		UpdateCurve ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
+
+	void CreateControlPoint(Vector3 position) {
+		RoadControlPoint newPoint = (RoadControlPoint)GameObject.Instantiate (controlPointTemplate);
+		newPoint.transform.position = position;
+		newPoint.road = this;
+		newPoint.transform.parent = this.transform;
+		controlPoints.Add(newPoint);
 	}
 
-	void UpdateCurve() {
+	public void UpdateCurve() {
 		for (int i = 0; i < SEGMENT_COUNT; i++) {
-			float t = i / (float)SEGMENT_COUNT;
+			float t = i / (float)(SEGMENT_COUNT - 1);
 
-			Vector3 point = CalculateBezier (t, points);
+			Vector3 point = CalculateBezier (t, controlPoints);
 			lineRenderer.SetPosition(i, point);
 		}
 	}
 
-	Vector3 CalculateBezier(float t, List<Vector3> points) {
+	Vector3 CalculateBezier(float t, List<RoadControlPoint> points) {
 		int count = points.Count;
 		if (count > 2) {
 			return (1 - t) * CalculateBezier(t, points.GetRange(0, count - 1)) * t + t * CalculateBezier(t, points.GetRange(1, count - 1)) * t;
 		} else {
-			return Vector3.Lerp (points [0], points [1], t);
+			return Vector3.Lerp (points [0].transform.position, points [1].transform.position, t);
 		}
 	}
 }
---
+++ src/AR_Sandbox/Assets/Scripts/RoadControlPoint.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoadControlPoint : MonoBehaviour {
+	public Road road;
+
+	// Use this for initialization
+	void Start () {
+		if (road == null) {
+			road = GetComponentInParent<Road> ();
+			if (road == null) {
+				Debug.LogError ("RoadControlPoint: no road to control!");
+			}
+		}
+	}
+
+	public void OnMouseDrag() {
+		Vector3 mousePos = Camera.main.ScreenToWorldPoint (Input.mousePosition);
+		transform.position = new Vector3 (mousePos.x, transform.position.y, mousePos.z);
+		road.UpdateCurve ();
+	}
+}
