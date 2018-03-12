6ae7580f9f31e67863e2482b512170df754770f7
Andrew Soltesz <ansolt@outlook.com>
2018-03-11 20:44:14 -0700

    Fixed road performance issues

--- src/AR_Sandbox/Assets/Scripts/RoadManager.cs
+++ src/AR_Sandbox/Assets/Scripts/RoadManager.cs
@@ -30,41 +30,43 @@ public class RoadManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // get altitude from ground
-        Vector3 Direction = transform.position - Ground.transform.position;
-        float altitude = Direction.y;
-        //Debug.Log("Altitude: " + altitude);
-
-        // get height from terrain
-        roadHeight = Mathf.Infinity;
-        RaycastHit hit;
-
-        // when above terrain change to blue material
-        // when below terrain change to red material
-        if (Physics.Raycast(transform.position, Vector3.down, out hit))
-            roadHeight = Vector3.Distance(hit.point, transform.position);
-        //Debug.Log("Height: " + roadHeight);
-        if (roadHeight == Mathf.Infinity)
-        {
-            material.color = Color.red;
-            GetComponent<Renderer>().material = material;
-        }
-        else
-        {
-            material.color = Color.blue;
-            GetComponent<Renderer>().material = material;
-        }
-
-        // testing get height function from terrain generator
-        terrainHeight = terrain.GetComponent<TerrainGenerator>();
-        roadHeight = terrainHeight.GetHeightAtWorldPosition(transform.position);
-
-        // Uncomment for cut/fill height data debug output 
-        //Debug.Log("GetHeightAtWorldPosition: " + roadHeight);
+       
     }
 
     public float getHeight()
     {
+		// get altitude from ground
+		Vector3 Direction = transform.position - Ground.transform.position;
+		float altitude = Direction.y;
+		//Debug.Log("Altitude: " + altitude);
+
+		// get height from terrain
+		roadHeight = Mathf.Infinity;
+		RaycastHit hit;
+
+		// when above terrain change to blue material
+		// when below terrain change to red material
+		if (Physics.Raycast(transform.position, Vector3.down, out hit))
+			roadHeight = Vector3.Distance(hit.point, transform.position);
+		//Debug.Log("Height: " + roadHeight);
+		if (roadHeight == Mathf.Infinity)
+		{
+			material.color = Color.red;
+			GetComponent<Renderer>().material = material;
+		}
+		else
+		{
+			material.color = Color.blue;
+			GetComponent<Renderer>().material = material;
+		}
+
+		// testing get height function from terrain generator
+		terrainHeight = terrain.GetComponent<TerrainGenerator>();
+		roadHeight = terrainHeight.GetHeightAtWorldPosition(transform.position);
+
+		// Uncomment for cut/fill height data debug output 
+		//Debug.Log("GetHeightAtWorldPosition: " + roadHeight);
+
         return roadHeight;
     }
 }
