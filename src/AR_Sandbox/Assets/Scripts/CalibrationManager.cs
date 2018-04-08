68e39be579b118276ae66b935533e4280c52ca48
sprouseamark <sprouseamark@gmail.com>
2018-04-08 15:24:22 -0700

    Added terrain adjustment on arrow press with slight cleanup

--- src/AR_Sandbox/Assets/Scripts/CalibrationManager.cs
+++ src/AR_Sandbox/Assets/Scripts/CalibrationManager.cs
@@ -5,27 +5,48 @@ using UnityEngine.UI;
 
 public class CalibrationManager : MonoBehaviour {
 	private Camera mainCamera;
-
+	public float terrainSpeed = .01f;
+	Vector3 move;
 	[SerializeField]
 	Transform UIPanel; //Will assign our panel to this variable so we can enable/disable it
 
 	TerrainManager terrainManager;
+
+	[SerializeField]
+	GameObject terrain;
 
 	// Use this for initialization
 	void Start () {
 		mainCamera = Camera.main;
 
 		terrainManager = GameObject.Find ("Terrain_Manager").GetComponent<TerrainManager> ();
-
 	}
-	
 	// Update is called once per frame
 	void Update () {
 		SubmitCalibration ();
+		if (ModeManager.dMode == DisplayMode.Calibrate) {
+			//Have arrow keys adjust the terrain
+			if (Input.GetKey (KeyCode.LeftArrow)) {
+				moveTerrainLeft ();
+			}
+			if (Input.GetKey (KeyCode.RightArrow)) {
+				moveTerrainRight ();
+			
+			}
+			if (Input.GetKey (KeyCode.UpArrow)) {
+				moveTerrainUp ();
+			}
+			if (Input.GetKey (KeyCode.DownArrow)) {
+				moveTerrainDown ();
+			}
+		}
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			Debug.Log(terrain.transform.position);
+		}
 	}
 
 	public void SubmitCalibration() {
-		int panelWidth, panelHeight, terrainWidth, terrainHeight;
 		Vector3 UpperRight, LowerLeft,UpperLeft,LowerRight;
 
 
@@ -43,5 +64,33 @@ public class CalibrationManager : MonoBehaviour {
 		Vector3 panelLR = mainCamera.ScreenToWorldPoint(LowerRight);
 
 		terrainManager.terrainMask.SetDimensions (panelLL, panelUR);
+	}
+
+	public void moveTerrainLeft()
+	{
+		float pos = terrain.transform.position.x;
+		pos = terrain.transform.position.x - terrainSpeed;
+		terrain.transform.position = new Vector3 (terrain.transform.position.x - terrainSpeed,terrain.transform.position.y,transform.position.z);
+	}
+
+	public void moveTerrainRight()
+	{
+		float pos = terrain.transform.position.x;
+		pos = terrain.transform.position.x + terrainSpeed;
+		terrain.transform.position = new Vector3 (terrain.transform.position.x + terrainSpeed,terrain.transform.position.y,transform.position.z);
+	}
+
+	public void moveTerrainUp()
+	{
+		float pos = terrain.transform.position.z;
+		pos = terrain.transform.position.z + terrainSpeed;
+		terrain.transform.position = new Vector3 (transform.position.x,terrain.transform.position.y,terrain.transform.position.z + terrainSpeed);
+	}
+
+	public void moveTerrainDown()
+	{
+		float pos = terrain.transform.position.z;
+		pos = terrain.transform.position.z - terrainSpeed;
+		terrain.transform.position = new Vector3 (transform.position.x,terrain.transform.position.y,terrain.transform.position.z - terrainSpeed);
 	}
 }
