99d4a059acb0865510e51dc9ad92b7d042418ec8
petroffr <petroffr@oregonstate.edu>
2018-04-15 15:00:21 -0700

    Cleanup code

--- src/AR_Sandbox/Assets/Scripts/RoadManager.cs
+++ src/AR_Sandbox/Assets/Scripts/RoadManager.cs
@@ -8,28 +8,21 @@ public class RoadManager : MonoBehaviour
     public GameObject terrain;
     private TerrainGenerator terrainHeight;
 
-    private float roadHeight;
-    public static Mesh viewedModel;
-    public GameObject Ground;
-    public Material material;
-
     public GameObject road;
     private Road roadPoint;
 
     // Use this for initialization
     void Start()
     {
-        // get terrain object mesh
+        // get terrain object
         terrain = GameObject.Find("Terrain");
+        // get height function from terrain generator
+        terrainHeight = terrain.GetComponent<TerrainGenerator>();
+
+        // get road object
         road = GameObject.Find("Road");
-        //MeshFilter viewedModelFilter = (MeshFilter)terrain.GetComponent("MeshFilter");
-        //viewedModel = viewedModelFilter.mesh;
-
-        // get terrain ground for raycasting
-        Ground = terrain;
-
-        // setup new material
-        material = new Material(Shader.Find("Unlit/RoadShader"));
+        // get point from road
+        roadPoint = road.GetComponent<Road>();
     }
 
     // Update is called once per frame
@@ -40,38 +33,11 @@ public class RoadManager : MonoBehaviour
 
     public float getHeight()
     {
-		// get altitude from ground
-		Vector3 Direction = transform.position - Ground.transform.position;
-		float altitude = Direction.y;
-		//Debug.Log("Altitude: " + altitude);
-
-		// get height from terrain
-		roadHeight = Mathf.Infinity;
-		RaycastHit hit;
-
-		// when above terrain change to blue material
-		// when below terrain change to red material
-		if (Physics.Raycast(transform.position, Vector3.down, out hit))
-			roadHeight = Vector3.Distance(hit.point, transform.position);
-		//Debug.Log("Height: " + roadHeight);
-		if (roadHeight == Mathf.Infinity)
-		{
-			material.color = Color.red;
-			GetComponent<Renderer>().material = material;
-		}
-		else
-		{
-			material.color = Color.blue;
-			GetComponent<Renderer>().material = material;
-		}
-
-        // testing get point from road
-        roadPoint = road.GetComponent<Road>();
         Vector3[] positions = roadPoint.GetRoadPoints();
 
-		// testing get height function from terrain generator
-		terrainHeight = terrain.GetComponent<TerrainGenerator>();
+        float roadHeight = 0f;
         int count = 0;
+
         //Debug.Log("Height at World Position: " + terrainHeight.GetHeightAtWorldPosition(positions[0]));
         foreach (Vector3 p in positions)
         {
