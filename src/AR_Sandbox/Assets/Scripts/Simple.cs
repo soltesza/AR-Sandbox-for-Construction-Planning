e7ab917b70b11366313d2178f284888a5aa332d2
petroffr <petroffr@oregonstate.edu>
2018-05-11 14:35:56 -0700

    add rows and columns to table

--- src/AR_Sandbox/Assets/Scripts/Simple.cs
+++ src/AR_Sandbox/Assets/Scripts/Simple.cs
@@ -12,27 +12,43 @@ public class Simple : MonoBehaviour
 
         this.table = this.GetComponent<Table>();
 
-        this.table.reset();
+        this.table.ResetTable();
 
-        this.table.addTextColumn();
-        this.table.addTextColumn();
-        this.table.addTextColumn();
+        this.table.AddTextColumn("Station");
+        this.table.AddTextColumn("Existing Gr");
+        this.table.AddTextColumn("Proposed Gr");
+        this.table.AddTextColumn("Roadway Width");
+        this.table.AddTextColumn("Cut Area");
+        this.table.AddTextColumn("Fill Area");
+        this.table.AddTextColumn("Cut Volumes");
+        this.table.AddTextColumn("Fill Volumes");
+        this.table.AddTextColumn("Adj. Fill Volumes");
+        this.table.AddTextColumn("Algebraic Sum");
+        this.table.AddTextColumn("Mass Ordinate");
 
         // Initialize Your Table
-        this.table.initialize(this.onTableSelected);
+        this.table.Initialize(this.onTableSelected);
 
         // Populate Your Rows (obviously this would be real data here)
-        for (int i = 0; i < 100; i++)
+        for (int i = 0; i < 20; i++)
         {
             Datum d = Datum.Body(i.ToString());
             d.elements.Add("Col1:Row" + i.ToString());
             d.elements.Add("Col2:Row" + i.ToString());
             d.elements.Add("Col3:Row" + i.ToString());
+            d.elements.Add("Col4:Row" + i.ToString());
+            d.elements.Add("Col5:Row" + i.ToString());
+            d.elements.Add("Col6:Row" + i.ToString());
+            d.elements.Add("Col7:Row" + i.ToString());
+            d.elements.Add("Col8:Row" + i.ToString());
+            d.elements.Add("Col9:Row" + i.ToString());
+            d.elements.Add("Col10:Row" + i.ToString());
+            d.elements.Add("Col11:Row" + i.ToString());
             this.table.data.Add(d);
         }
 
         // Draw Your Table
-        this.table.startRenderEngine();
+        this.table.StartRenderEngine();
 
     }
 
