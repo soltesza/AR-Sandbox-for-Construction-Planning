6891994e69d27b5ab19c5486a8b0a9e56ce1dffd
sprouseamark <sprouseamark@gmail.com>
2018-06-04 14:46:01 -0700

    Added comments to ModeManager and cleaned up code

--- src/AR_Sandbox/Assets/Scripts/ModeManager.cs
+++ src/AR_Sandbox/Assets/Scripts/ModeManager.cs
@@ -3,14 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 //Use this to keep track of what state we are in
-//Should other objects find the current state here?  Should this manager what is called for each state?
-
 public enum DisplayMode : short {Depth, CutFill, Design, Calibrate};
 
 public class ModeManager : MonoBehaviour {
 
-    public static ModeManager instance = null;
-    public static DisplayMode dMode = DisplayMode.Depth;
+    public static ModeManager instance = null;		      //Used to ensure object is Singleton
+    public static DisplayMode dMode = DisplayMode.Depth;  //Current Display Mode
 
 	[SerializeField]
 	GameObject HeightView; 
@@ -35,56 +33,36 @@ public class ModeManager : MonoBehaviour {
 	
 	void Update () {
 		switch (dMode) {
-            case DisplayMode.Depth:     //What should occur while in Depth Mode?
-			    //Debug.Log("I am in Depth mode");
+            case DisplayMode.Depth:     //What should occur while in Depth Mode
 				terrainManager.SetTerrainTheme(TerrainManager.TerrainTheme.rainbow);
-				disableRoad();
+				road.gameObject.SetActive (false);
                 break;
-			case DisplayMode.CutFill:   //What should occur while in CutFill Mode?
-                 // Debug.Log("I am in CutFill mode");
+			case DisplayMode.CutFill:   //What should occur while in CutFill Mode
 				terrainManager.SetTerrainTheme (TerrainManager.TerrainTheme.greyscale);
-				//disableTerrain();
-				enableRoad ();
+				road.gameObject.SetActive (true);
 				road.DisableControlPoints ();
 				break;
-            case DisplayMode.Calibrate: //What should occur while in Calibrate Mode?
-                // Debug.Log("I am in Calibrate mode");
+            case DisplayMode.Calibrate: //What should occur while in Calibrate Mode
 				terrainManager.SetTerrainTheme(TerrainManager.TerrainTheme.rainbow);
-				disableRoad();
+				road.gameObject.SetActive (false);
 				break;
-		case DisplayMode.Design:    //What should occur while in Design Mode?
-			terrainManager.SetTerrainTheme (TerrainManager.TerrainTheme.greyscale);
-			enableRoad ();
-			road.EnableControlPoints ();
-			if ((Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl)) && Input.GetKeyDown (KeyCode.Z)) {
-				road.Undo ();		
-			}
+			case DisplayMode.Design:    //What should occur while in Design Mode
+				terrainManager.SetTerrainTheme (TerrainManager.TerrainTheme.greyscale);
+				road.gameObject.SetActive (true);
+				road.EnableControlPoints ();
+				if ((Input.GetKey (KeyCode.LeftControl) || Input.GetKey (KeyCode.RightControl)) && Input.GetKeyDown (KeyCode.Z)) {
+					road.Undo ();		
+				}
 
-			if (Input.GetKeyDown (KeyCode.LeftShift)) {
-				HeightView.SetActive (true);
-			}
-			if (Input.GetKeyUp (KeyCode.LeftShift)) {
-				HeightView.SetActive (false);
-			}
+				if (Input.GetKeyDown (KeyCode.LeftShift)) {
+					HeightView.SetActive (true);
+				}
+				if (Input.GetKeyUp (KeyCode.LeftShift)) {
+					HeightView.SetActive (false);
+				}
 
-                break;
+	                break;
         }
 	}
 		
-	void enableTerrain() {
-		terrainManager.terrainGenerator.gameObject.SetActive (true);
-	}
-		
-	void disableTerrain() {
-		terrainManager.terrainGenerator.gameObject.SetActive (false);
-	}
-		
-	void enableRoad() {
-		road.gameObject.SetActive (true);
-	}
-		
-	void disableRoad() {
-		road.gameObject.SetActive (false);
-	}
-
 }
