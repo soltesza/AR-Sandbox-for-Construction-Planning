cd7a43322780072bd12747058c3199227120e957
petroffr <petroffr@oregonstate.edu>
2018-06-10 21:47:04 -0700

    Cleaned up and added more comments to CutAndFillManager

--- src/AR_Sandbox/Assets/Scripts/CutAndFillManager.cs
+++ src/AR_Sandbox/Assets/Scripts/CutAndFillManager.cs
@@ -11,10 +11,8 @@ public class CutAndFillManager : MonoBehaviour {
     // max points for the table
     const int MAXPOINTS = 20;
 
-    // setup arrays for cut/fill data, mass haul data and equipment
-    // distances between stations
+    // setup arrays for cut/fill data and mass haul data
     float[] station       = new float[MAXPOINTS];
-    // grade data
     float[] existGrade    = new float[MAXPOINTS];
     float[] propGrade     = new float[MAXPOINTS];
     float[] roadWidth     = new float[MAXPOINTS];
@@ -25,21 +23,23 @@ public class CutAndFillManager : MonoBehaviour {
     float[] adjFillVolume = new float[MAXPOINTS];
     float[] algebraicSum  = new float[MAXPOINTS];
     float[] massOrdinate  = new float[MAXPOINTS];
-    // construction equipment
-    //string[] equipment = { "Dozer", "Wheel Loaders", "Scraper", "Articulated Truck", "Rear Dump Truck", "Wagon" };
 
     [SerializeField]
     Transform UIPanel; //Will assign our panel to this variable so we can enable/disable it
 
+    // variables for getting the terrain object and terrain height
     public GameObject terrain;
     private TerrainGenerator terrainHeight;
 
+    // variables for getting the road object and road points
     public GameObject road;
     private Road roadPoint;
 
+    // timer variables, used for testing table update function
     float timer = 0f;
     float waitingTime = 5f;
 
+    // handle to actual data table
     private Table table;
 
     void Start()
@@ -54,6 +54,7 @@ public class CutAndFillManager : MonoBehaviour {
         // get point from road
         roadPoint = road.GetComponent<Road>();
 
+        // get the table attached to the cut/fill panel
         table = GetComponent<Table>();
 
         updateTable();
@@ -92,8 +93,10 @@ public class CutAndFillManager : MonoBehaviour {
 
     void updateTable()
     {
+        // reset the table before updating it
         table.ResetTable();
 
+        // add the column headers
         table.AddTextColumn("Station (ft)");
         table.AddTextColumn("Existing Gr (ft)");
         table.AddTextColumn("Proposed Gr (ft)");
@@ -109,7 +112,7 @@ public class CutAndFillManager : MonoBehaviour {
         // Initialize Your Table
         table.Initialize(onTableSelected);
 
-        // setup values
+        // update values
         updateStation();
         updateExistGrade();
         updatePropGrade();
@@ -146,12 +149,15 @@ public class CutAndFillManager : MonoBehaviour {
 
     void updateStation()
     {
+        // first station is always zero
         station[0] = 0;
 
+        // get the road points from the road object
         Vector3[] positions = roadPoint.GetRoadPoints();
 
         for (int i = 1; i < MAXPOINTS; i++)
         {
+            // calcuate the station distances using vector distance function
             station[i] = station[i - 1] +  Vector3.Distance(positions[i - 1], positions[i]);
             //Debug.Log("STATION #" + i + " value " + station[i]);
         }
@@ -163,6 +169,7 @@ public class CutAndFillManager : MonoBehaviour {
 
         for (int i = 0; i < MAXPOINTS; i++)
         {
+            // existing grade is basically the road distance from the terrain
             existGrade[i] = terrainHeight.GetHeightAtWorldPosition(positions[i]);
             //Debug.Log("EXISTING GRADE #" + i + " value " + existGrade[i]);
         } 
@@ -172,6 +179,7 @@ public class CutAndFillManager : MonoBehaviour {
     {
         for (int i = 0; i < MAXPOINTS; i++)
         {
+            // proposed grade or road height after cut/fill, can be changed in the future
             propGrade[i] = 0;
             //Debug.Log("PROPOSED GRADE #" + i + " value " + propGrade[i]);
         }
@@ -181,6 +189,7 @@ public class CutAndFillManager : MonoBehaviour {
     {
         for (int i = 0; i < MAXPOINTS; i++)
         {
+            // width of the road, can be changed in the future
             roadWidth[i] = 120;
             //Debug.Log("ROAD WIDTH #" + i + " value " + roadWidth[i]);
         }
@@ -190,6 +199,7 @@ public class CutAndFillManager : MonoBehaviour {
     {
         for (int i = 0; i < MAXPOINTS; i++)
         {
+            // if terrain above road, then cut
             if (existGrade[i] > 0)
             {
                 cutArea[i] = (existGrade[i] * existGrade[i]) + (roadWidth[i] * existGrade[i]);
@@ -206,6 +216,7 @@ public class CutAndFillManager : MonoBehaviour {
     {
         for (int i = 0; i < MAXPOINTS; i++)
         {
+            // if terrain below road, then fill
             if (existGrade[i] <= 0)
             {
                 fillArea[i] = -1 * ((existGrade[i] * existGrade[i]) + Mathf.Abs(roadWidth[i] * existGrade[i]));
@@ -224,6 +235,7 @@ public class CutAndFillManager : MonoBehaviour {
 
         for (int i = 1; i < MAXPOINTS; i++)
         {
+            // use cut area to calculate cut volume
             cutVolume[i] = (cutArea[i] + cutArea[i - 1]) / 2 * (station[i] - station[i - 1]) / 27;
             //Debug.Log("CUT VOLUME #" + i + " value " + cutVolume[i]);
         }
@@ -235,6 +247,7 @@ public class CutAndFillManager : MonoBehaviour {
 
         for (int i = 1; i < MAXPOINTS; i++)
         {
+            // use fill area to calculate fill volume
             fillVolume[i] = (fillArea[i] + fillArea[i - 1]) / 2 * (station[i] - station[i - 1]) / 27;
             //Debug.Log("FILL VOLUME #" + i + " value " + fillVolume[i]);
         }
@@ -244,6 +257,7 @@ public class CutAndFillManager : MonoBehaviour {
     {
         for (int i = 0; i < MAXPOINTS; i++)
         {
+            // adjusted fill volume (convert from compacted cubic yards to bank cubic yards)
             adjFillVolume[i] = fillVolume[i] / 0.9f;
             //Debug.Log("ADJUSTED FILL VOLUME #" + i + " value " + adjFillVolume[i]);
         }
@@ -253,6 +267,7 @@ public class CutAndFillManager : MonoBehaviour {
     {
         for (int i = 0; i < MAXPOINTS; i++)
         {
+            // cut vol - fill vol (algebraicSum is used to calculate mass ordinates)
             algebraicSum[i] = cutVolume[i] + adjFillVolume[i];
             //Debug.Log("ALGEBRAIC SUM #" + i + " value " + algebraicSum[i]);
         }
@@ -264,6 +279,10 @@ public class CutAndFillManager : MonoBehaviour {
 
         for (int i = 1; i < MAXPOINTS; i++)
         {
+            /* (cumulative sum, aka mass ordinate)
+             * Used to figure out how much excess and deficit material available after 
+             * all cut material used for required fill in each section.
+             */
             massOrdinate[i] = massOrdinate[i - 1] + algebraicSum[i];
             //Debug.Log("MASS ORDINATE #" + i + " value " + massOrdinate[i]);
         }
