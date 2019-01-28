c46154b610f73d835a766108ae1acc4605273232
Adam  Sunderman <sunderad@oregonstate.edu>
2019-01-27 18:57:08 -0800

    Added the AR_Sumobox Unity project READ DESCRIPTION
    This is the Unity project I have at this point. It also requires a modified Sumo package to be installed at C://Sumo. The modified Sumo package and a readme with all needed info is on the way soon.

---
+++ src/AR_Sumobox/Assets/Scripts/SumoCreator.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.IO;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Diagnostics;
+
+public class SumoCreator : MonoBehaviour
+{
+
+    public void CreateSumoConfigTemplate()
+    {
+        Process p = new Process();
+        ProcessStartInfo si = new ProcessStartInfo();
+        si.WindowStyle = ProcessWindowStyle.Hidden;
+        si.FileName = "cmd.exe";
+        si.Arguments = "/C sumo --save-template Assets/Generated/Config/temp.sumocfg --save-commented";
+        p.StartInfo = si;
+        p.Start();
+        p.WaitForExit(5000);
+    }
+
+    public void GenerateOsmNetwork()
+    {
+        try
+        {
+            Process p = new Process();
+            ProcessStartInfo si = new ProcessStartInfo();
+            si.FileName = "cmd.exe";
+            si.Arguments = "/C osmWebWizard.py";
+            si.UseShellExecute = false;
+            si.RedirectStandardOutput = true;
+            si.CreateNoWindow = true;
+            p.StartInfo = si;
+            p.Start();
+            while (!p.StandardOutput.EndOfStream)
+            {
+                string last = p.StandardOutput.ReadLine();
+                UnityEngine.Debug.Log(last);
+                if (last.Contains("osm.sumocfg"))
+                {
+                    if(!p.HasExited)
+                    {
+                        p.Kill();
+                    }
+                    break;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            UnityEngine.Debug.LogError(e.Message);
+        }
+    }  
+}
