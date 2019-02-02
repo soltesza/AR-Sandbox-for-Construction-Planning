cffaba299ee8d7cc7a659beda5d8420cb7bf9c97
Adam  Sunderman <sunderad@oregonstate.edu>
2019-02-02 14:39:47 -0800

    Changed the Unity Scripts to work with the new OSM import.
    As of now the processes show their windows show we can remember that at some point we need to close them gracfully.

--- src/AR_Sumobox/Assets/Scripts/SumoCreator.cs
+++ src/AR_Sumobox/Assets/Scripts/SumoCreator.cs
@@ -8,18 +8,6 @@ using System.Diagnostics;
 public class SumoCreator : MonoBehaviour
 {
 
-    public void CreateSumoConfigTemplate()
-    {
-        Process p = new Process();
-        ProcessStartInfo si = new ProcessStartInfo();
-        si.WindowStyle = ProcessWindowStyle.Hidden;
-        si.FileName = "cmd.exe";
-        si.Arguments = "/C sumo --save-template Assets/Generated/Config/temp.sumocfg --save-commented";
-        p.StartInfo = si;
-        p.Start();
-        p.WaitForExit(5000);
-    }
-
     public void GenerateOsmNetwork()
     {
         try
@@ -27,13 +15,16 @@ public class SumoCreator : MonoBehaviour
             Process p = new Process();
             ProcessStartInfo si = new ProcessStartInfo();
             si.FileName = "cmd.exe";
-            si.Arguments = "/C osmWebWizard.py";
-            si.UseShellExecute = false;
-            si.RedirectStandardOutput = true;
-            si.CreateNoWindow = true;
+            si.Arguments = "/C osmWebWizard.py --remote --address=localhost --port=80";
             p.StartInfo = si;
             p.Start();
-            p.WaitForExit();
+
+            Process p2 = new Process();
+            ProcessStartInfo si2 = new ProcessStartInfo();
+            si2.FileName = "cmd.exe";
+            si2.Arguments = "/C C:/Sumo/tools/webWizard/index.html";
+            p2.StartInfo = si2;
+            p2.Start();
         }
         catch (Exception e)
         {
