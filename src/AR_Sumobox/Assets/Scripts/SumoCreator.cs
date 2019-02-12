c15ad82b7f23f83957fc7fc8db3b758028fc4bc4
Adam  Sunderman <sunderad@oregonstate.edu>
2019-02-11 21:54:59 -0800

    Ready for road building
    Cleaned up some code, removed unneccessary functions, added data stucts for info that will be likly be needed later, and got the road building function stubbed out and ready using the new structs. These data stucts will be available across the project while the sim is loaded. Check the code for their handles (line 59).

--- src/AR_Sumobox/Assets/Scripts/SumoCreator.cs
+++ src/AR_Sumobox/Assets/Scripts/SumoCreator.cs
@@ -8,69 +8,74 @@ using System.Globalization;
 using UnityEngine;
 using UnityEditor;
 
+public struct ProjectionData
+{
+    public string offset;
+    public string originalBounds;
+    public string projectedBounds;
+}
+
+public struct Lane
+{
+    public string id;
+    public string index;
+    public string speed;
+    public string length;
+    public string width;
+    public string allow;
+    public string disallow;
+    public string shape;
+}
+
+public struct Edge
+{
+    public string id;
+    public string from;
+    public string to;
+    public string name;
+    public string shape;
+    public string type;
+    public List<Lane> lanes;
+}
+
+public struct Junction
+{
+    public string id;
+    public string name;
+    public string type;
+    public string x;
+    public string y;
+    public string incomingLanes;
+    public string internalLanes;
+    public string shape;
+}
+
 public class SumoCreator : MonoBehaviour
 {
-    // A handle to the Terrain Plane
+
+    // A handle to the Terrain Plane and Network parent GameObject
     public GameObject Terrain_Plane;
     public GameObject Network_Parent;
+    public ProjectionData Projection_Data;
+    public List<Junction> Junctions;
+    public List<Edge> Edges;
 
-    void Start()
+    private void Start()
     {
         Network_Parent = new GameObject();
     }
 
-    // Lets a user pick a generated network with a file selection prompt.
-    private string[] OpenFileSelectionDialog()
+    // Gets the bounds of the current network as a pair of points in 2-Space
+    private ProjectionData GetProjectionData(XmlDocument xml)
     {
-        try
+        XmlNode location = xml.DocumentElement.SelectSingleNode("location");
+        ProjectionData ret = new ProjectionData()
         {
-            string path = EditorUtility.OpenFolderPanel("Select a SUMO Network Folder.", "", "");
-            string[] files = Directory.GetFiles(path);
-            return files;
-        }
-        catch (Exception e)
-        {
-            UnityEngine.Debug.LogException(e);
-            return null;
-        }
-    }
-
-    // Opens and returns an XML document
-    private XmlDocument OpenXML(string filename)
-    {
-        try
-        {
-            XmlDocument x = new XmlDocument();
-            x.Load(filename);
-            return x;
-        }
-        catch (Exception e)
-        {
-            UnityEngine.Debug.LogException(e);
-            return null;
-        }
-    }
-
-    // Gets the bounds of the current network as two pairs of points in 2-Space
-    private List<float> GetNetworkBounds(XmlDocument xml)
-    {
-        try
-        {
-            XmlNode location = xml.DocumentElement.SelectSingleNode("location");
-            string[] bounds = location.Attributes.GetNamedItem("convBoundary").Value.Split(',');
-            List<string> boundarys = bounds.ToList();
-            List<float> boundaryPoints = new List<float>();
-            foreach (string b in boundarys)
-            {
-                boundaryPoints.Add(float.Parse(b, CultureInfo.InvariantCulture.NumberFormat));
-            }
-            return boundaryPoints;
-        }
-        catch (Exception e)
-        {
-            UnityEngine.Debug.LogException(e);
-            return null;
-        }  
+            offset = location.Attributes.GetNamedItem("netOffset").Value,
+            originalBounds = location.Attributes.GetNamedItem("origBoundary").Value,
+            projectedBounds = location.Attributes.GetNamedItem("convBoundary").Value
+        }; 
+        return ret;
     }
 
     // Sumo shape sting to List of floats point order is
@@ -91,27 +96,33 @@ public class SumoCreator : MonoBehaviour
     }
 
     // Adds a Terrain_Plane prefab to the scene 100 units (meters) larger than the network.
-    private void BuildTerrain(List<float> bp)
+    private void BuildTerrain(ProjectionData PD)
     {
-        float x = Math.Abs(bp[2] - bp[0]) + 100.0f;
+        List<float> bp = ShapeStringToFloatList(PD.projectedBounds);
+        float x = (Math.Abs(bp[2] - bp[0]) + 100.0f) * 0.15f;
         float y = 1.0f;
-        float z = Math.Abs(bp[3] - bp[1]) + 100.0f;
+        float z = (Math.Abs(bp[3] - bp[1]) + 100.0f) * 0.15f;
         Vector3 scale = new Vector3(x, y, z);
-        GameObject terrain = Instantiate(Terrain_Plane, new Vector3(0.0f, 0.0f, 0.0f), Quaternion.identity);
+        GameObject terrain = Instantiate(Terrain_Plane, new Vector3(x/2.0f, 0.0f, y/2.0f), Quaternion.identity);
         terrain.transform.localScale = scale;
         terrain.SetActive(true);
+        GameObject.Find("Main_Camera").transform.SetPositionAndRotation(new Vector3(x / 2.0f, 100.0f, y / 2.0f), Quaternion.Euler(new Vector3(90.0f,0.0f,0.0f)));
     }
 
     // Builds Road Junctions
-    private void Build_Junction(string x, string y, string shape)
+    private void BuildJunction(Junction J)
     {
         // String points to floats
-        List<float> fshape = ShapeStringToFloatList(shape);
-        int numverts = fshape.Count()/2;
+        if (J.shape == null || J.type == "internal")
+        {
+            return;
+        }
+        List<float> fshape = ShapeStringToFloatList(J.shape);
+        int numverts = fshape.Count() / 2;
 
         // Center of junction
-        float xjunc = float.Parse(x, CultureInfo.InvariantCulture.NumberFormat);
-        float yjunc = float.Parse(y, CultureInfo.InvariantCulture.NumberFormat);
+        float xjunc = float.Parse(J.x, CultureInfo.InvariantCulture.NumberFormat);
+        float yjunc = float.Parse(J.y, CultureInfo.InvariantCulture.NumberFormat);
         Vector3 centerpoint = new Vector3(xjunc, yjunc, 0.2f);
 
         if (numverts > 5)
@@ -120,12 +131,12 @@ public class SumoCreator : MonoBehaviour
             GameObject chunk = new GameObject();
             chunk.AddComponent<MeshRenderer>();
             Material m = chunk.GetComponent<MeshRenderer>().material;
-            m.color = new Vector4(0.5f,0.5f,0.5f,1.0f);
-            m.SetFloat("Smoothness", 0.0f);
+            m.color = new Vector4(0.5f, 0.5f, 0.5f, 1.0f);
+            m.SetFloat("Smoothness", 1.0f);
             Mesh mesh = new Mesh();
 
             // Build Vertices
-            Vector3[] verts = new Vector3[numverts+1];
+            Vector3[] verts = new Vector3[numverts + 1];
             int vc = 0;
             for (int i = 0; i < fshape.Count(); i = i + 2)
             {
@@ -139,11 +150,11 @@ public class SumoCreator : MonoBehaviour
             int[] tris = new int[(verts.Length - 1) * 3];
             int triscounter = 0;
             int trisindex = 0;
-            for(int k = 0; k < verts.Length - 1; k++)
+            for (int k = 0; k < verts.Length - 1; k++)
             {
                 tris[trisindex] = triscounter;
-                tris[trisindex+1] = verts.Length - 1;
-                tris[trisindex+2] = triscounter+1;
+                tris[trisindex + 1] = verts.Length - 1;
+                tris[trisindex + 2] = triscounter + 1;
                 triscounter++;
                 trisindex += 3;
             }
@@ -153,7 +164,7 @@ public class SumoCreator : MonoBehaviour
             mesh.triangles = tris;
 
             // Build Normals
-            Vector3[] norms = new Vector3[numverts+1];
+            Vector3[] norms = new Vector3[numverts + 1];
             for (int k = 0; k < norms.Length; k++)
             {
                 norms[k] = Vector3.up;
@@ -161,87 +172,168 @@ public class SumoCreator : MonoBehaviour
             mesh.normals = norms;
 
             chunk.AddComponent<MeshFilter>().mesh = mesh;
-            chunk.isStatic = false;
+            chunk.isStatic = true;
             chunk.transform.Rotate(new Vector3(-90.0f, 0.0f, 0.0f));
-            chunk.transform.Translate(new Vector3(centerpoint.x * 5.0f, centerpoint.z, centerpoint.y * 5.0f), Space.World);
-            chunk.transform.localScale = chunk.transform.localScale * 5.0f;
+            chunk.transform.Translate(new Vector3(centerpoint.x, 0.2f, centerpoint.y), Space.World);
+            //chunk.transform.localScale = chunk.transform.localScale * 15.0f;
             chunk.transform.parent = Network_Parent.transform;
         }
     }
 
-    // Builds the network pieces
+    // Builds Road Pieces
+    private void BuildRoad(Edge edge)
+    {
+        
+    }
+
+    // Builds the network pieces by reading a Sumo network file
+    // Reads through a network and saves all the network data to structs.
+    // There are structs for projection parameters, edges, lanes and junctions.
+    // After all data is read each struct will be looped through and its shape 
+    // will be built with the according Build<PART>() function.
+    // See BuildRoad(), BuildTerrain(), BuildJunction() above.
     private void BuildNetwork(string file)
     {
         // Open the file
-        XmlDocument xmlDoc = OpenXML(file);
-        if (xmlDoc != null)
+        bool opened = true;
+        XmlDocument xmlDoc = new XmlDocument();
+        try
+        {
+            xmlDoc.Load(file);
+        }
+        catch (Exception e)
+        {
+            opened = false;
+            UnityEngine.Debug.LogException(e);
+        }
+        if (opened)
         {
             // Get the network size information from the 'location' 
             // node and build the terrain
-            List<float> boundaryPoints = GetNetworkBounds(xmlDoc);
-            BuildTerrain(boundaryPoints);
+            Projection_Data = GetProjectionData(xmlDoc);
+            BuildTerrain(Projection_Data);
 
+            Junctions = new List<Junction>();
+            Edges = new List<Edge>();
             XmlNodeList junctions = xmlDoc.DocumentElement.SelectNodes("junction");
             foreach (XmlNode junction in junctions)
             {
-                string x = null;
-                string y = null;
-                string jshape = null;
+                Junction theJunction = new Junction();
+
+                if (junction.Attributes["id"] != null)
+                {
+                    theJunction.id = junction.Attributes.GetNamedItem("id").Value;
+                }
+                if (junction.Attributes["name"] != null)
+                {
+                    theJunction.name = junction.Attributes.GetNamedItem("name").Value;
+                }
+                if (junction.Attributes["type"] != null)
+                {
+                    theJunction.type = junction.Attributes.GetNamedItem("type").Value;
+                }
+                else
+                {
+                    theJunction.type = "normal";
+                }
                 if (junction.Attributes["x"] != null)
                 {
-                    x = junction.Attributes.GetNamedItem("x").Value;
+                    theJunction.x = junction.Attributes.GetNamedItem("x").Value;
                 }
                 if (junction.Attributes["y"] != null)
                 {
-                    y = junction.Attributes.GetNamedItem("y").Value;
+                    theJunction.y = junction.Attributes.GetNamedItem("y").Value;
                 }
                 if (junction.Attributes["shape"] != null)
                 {
-                    jshape = junction.Attributes.GetNamedItem("shape").Value;
+                    theJunction.shape = junction.Attributes.GetNamedItem("shape").Value;
                 }
-                if(x != null && y != null && jshape != null)
-                {
-                    Build_Junction(x, y, jshape);
-                    //UnityEngine.Debug.Log(x + " " + y + " " + jshape);
-                }
-                
+                Junctions.Add(theJunction);
             }
 
             // Get all the edges in the network 
             XmlNodeList edges = xmlDoc.DocumentElement.SelectNodes("edge");
             foreach (XmlNode edge in edges)
             {
-                // Skip internal edges but get the children of the rest
-                if (edge.Attributes.GetNamedItem("function") != null && edge.Attributes.GetNamedItem("function").Value != "internal")
+                Edge theEdge = new Edge();
+                if (edge.Attributes["id"] != null)
                 {
-                    // Children are lanes, get shape, width and length info if it exists
-                    XmlNodeList lanes = edge.ChildNodes;
-                    foreach (XmlNode lane in lanes)
+                    theEdge.id = edge.Attributes.GetNamedItem("id").Value;
+                }
+                if (edge.Attributes["name"] != null)
+                {
+                    theEdge.name = edge.Attributes.GetNamedItem("name").Value;
+                }
+                if (edge.Attributes["from"] != null)
+                {
+                    theEdge.from = edge.Attributes.GetNamedItem("from").Value;
+                }
+                if (edge.Attributes["to"] != null)
+                {
+                    theEdge.to = edge.Attributes.GetNamedItem("to").Value;
+                }
+                if (edge.Attributes["type"] != null)
+                {
+                    theEdge.type = edge.Attributes.GetNamedItem("type").Value;
+                }
+                if (edge.Attributes["shape"] != null)
+                {
+                    theEdge.shape = edge.Attributes.GetNamedItem("shape").Value;
+                }
+
+                theEdge.lanes = new List<Lane>();
+                XmlNodeList lanes = edge.ChildNodes;
+                foreach (XmlNode lane in lanes)
+                {
+                    Lane theLane = new Lane();
+                    if (lane.Attributes["id"] != null)
                     {
-                        string length = null;
-                        string width = null; 
-                        string eshape = null;
-                        if (lane.Attributes["length"] != null)
-                        {
-                            length = lane.Attributes.GetNamedItem("length").Value;
-                        }
-                        if (lane.Attributes["width"] != null)
-                        {
-                            width = lane.Attributes.GetNamedItem("width").Value;
-                        }
-                        if (lane.Attributes["shape"] != null)
-                        {
-                            eshape = lane.Attributes.GetNamedItem("shape").Value;
-                        }
+                        theLane.id = lane.Attributes.GetNamedItem("id").Value;
                     }
+                    if (lane.Attributes["width"] != null)
+                    {
+                        theLane.width = lane.Attributes.GetNamedItem("width").Value;
+                    }
+                    if (lane.Attributes["index"] != null)
+                    {
+                        theLane.index = lane.Attributes.GetNamedItem("index").Value;
+                    }
+                    if (lane.Attributes["speed"] != null)
+                    {
+                        theLane.speed = lane.Attributes.GetNamedItem("speed").Value;
+                    }
+                    if (lane.Attributes["length"] != null)
+                    {
+                        theLane.length = lane.Attributes.GetNamedItem("length").Value;
+                    }
+                    if (lane.Attributes["allow"] != null)
+                    {
+                        theLane.allow = lane.Attributes.GetNamedItem("allow").Value;
+                    }
+                    if (lane.Attributes["disallow"] != null)
+                    {
+                        theLane.disallow = lane.Attributes.GetNamedItem("disallow").Value;
+                    }
+                    if (lane.Attributes["shape"] != null)
+                    {
+                        theLane.shape = lane.Attributes.GetNamedItem("shape").Value;
+                    }
+                    theEdge.lanes.Add(theLane);
                 }
-                else
-                {
-                    continue;
-                }
+                Edges.Add(theEdge);
+            }
+            // Build Roads here, Maybe move junctions here too
+            foreach(Edge e in Edges)
+            {
+                BuildRoad(e);
+            }
+            foreach(Junction j in Junctions)
+            {
+                BuildJunction(j);
             }
         }
     }
+
 
     // Open Up OSMWebWizard and let the user build a real network.
     // The user will save the new network to a zipfile when done.
@@ -281,46 +373,59 @@ public class SumoCreator : MonoBehaviour
     // in case we decide we need access to them later.
     public void LoadNetwork()
     {
-        string[] files = OpenFileSelectionDialog();
-        foreach (string file in files)
+        string[] files = null;
+        // Lets a user pick a generated network with a file selection prompt.
+        try
         {
-            // The trips files.
-            if (file.EndsWith(".trips.xml"))
+            string path = EditorUtility.OpenFolderPanel("Select a SUMO Network Folder.", "", "");
+            files = Directory.GetFiles(path);
+        }
+        catch (Exception e)
+        {
+            UnityEngine.Debug.LogException(e);
+        }
+        
+        if(files != null)
+        {
+            foreach (string file in files)
             {
-                continue;
-            }
-            // The routes files.
-            else if (file.EndsWith(".rou.xml"))
-            {
-                continue;
-            }
-            // The network file.
-            else if (file.EndsWith(".net.xml"))
-            {
-                BuildNetwork(file);
-            }
-            // The polygon file.
-            else if (file.EndsWith(".poly.xml"))
-            {
-                continue;
-            }
-            // The view file.
-            else if (file.EndsWith(".view.xml"))
-            {
-                continue;
-            }
-            // The config file.
-            else if (file.EndsWith(".sumocfg"))
-            {
-                continue;
-            }
-            else
-            {
-                // Ignore all batch files
-                if (!file.EndsWith(".bat"))
+                // The trips files.
+                if (file.EndsWith(".trips.xml"))
                 {
-                    string extmess = "Unknown File Extention " + file;
-                    UnityEngine.Debug.Log(extmess);
+                    continue;
+                }
+                // The routes files.
+                else if (file.EndsWith(".rou.xml"))
+                {
+                    continue;
+                }
+                // The network file.
+                else if (file.EndsWith(".net.xml"))
+                {
+                    BuildNetwork(file);
+                }
+                // The polygon file.
+                else if (file.EndsWith(".poly.xml"))
+                {
+                    continue;
+                }
+                // The view file.
+                else if (file.EndsWith(".view.xml"))
+                {
+                    continue;
+                }
+                // The config file.
+                else if (file.EndsWith(".sumocfg"))
+                {
+                    continue;
+                }
+                else
+                {
+                    // Ignore all batch files
+                    if (!file.EndsWith(".bat"))
+                    {
+                        UnityEngine.Debug.Log("Unknown File Extention " + file.ToString());
+                    }
                 }
             }
         }
