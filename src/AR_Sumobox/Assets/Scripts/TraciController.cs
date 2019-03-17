fa75fb41aa3feec7af92f3a94cda795ea2156598
jonah1217 <spencjon@oregonstate.edu>
2019-03-17 01:04:57 -0700

    Added TraciController - Building

--- src/AR_Sumobox/Assets/Scripts/TraciController.cs
+++ src/AR_Sumobox/Assets/Scripts/TraciController.cs
@@ -1,14 +1,50 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
+using Traci = CodingConnected.TraCI.NET;
 
 public class TraciController : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public GameObject Cars_GO;
+    public float speed = 2.0f;
+    public Traci.TraCIClient Client;
+    private Traci.TraCISubscriptionResponse Subscriptions = new Traci.TraCISubscriptionResponse();
+
     void Start()
     {
-        
+        Cars_GO = GameObject.Find("Cars");
     }
+    
+    void ConnectToSumo(string Hostname, int Port)
+    {
+        List<byte> carInfo = new List<byte> { Traci.TraCIConstants.POSITION_3D };
+        try
+        {
+            //Connect to sumo running on specified port
+            Client = new Traci.TraCIClient();
+            Client.Connect(Hostname, Port);
+
+            //Get all the car ids we need to keep track of. 
+            Traci.TraCIResponse<List<String>> CarIds = Client.Vehicle.GetIdList();
+            
+            // Subscribe to all cars
+            CarIds.Content.ForEach(car => Client.Vehicle.Subscribe(car, 0, 2147483647, carInfo));
+        }
+        catch(Exception e)
+        {
+            UnityEngine.Debug.LogError(e.Message);
+        }
+    }
+
+    Traci.Types.SubscriptionEventArgs HandleVehicleUpdate()
+    {
+        //Traci.Types.SubscriptionEventArgs Event = 
+        return new Traci.Types.SubscriptionEventArgs();
+    }
+
 
     // Update is called once per frame
     void Update()
